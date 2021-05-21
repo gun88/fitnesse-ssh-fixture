f6921885b1b484c8f34b3fe72071a1aa04ee672c
tpomante <tommaso.pomante@hpecds.com>
2021-05-21 13:58:42 +0200

    .

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     readmeUrl: 'https://raw.githubusercontent.com/gun88/fitnesse-ssh-fixture/master/README.md',
-    downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.1.0/fitnesse-ssh-fixture-1.1.0.jar',
+  downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.0.0/fitnesse-ssh-fixture-1.0.0.jar',
     gitHubUrl: 'https://github.com/gun88/fitnesse-ssh-fixture',
     wikiUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/wiki'
   }
