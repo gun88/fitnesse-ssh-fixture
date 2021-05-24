9b09c6bebcb022fcc4c1383ed0cb13d4ba85d820
tpomante <tommaso.pomante@hpecds.com>
2021-05-24 17:09:14 +0200

    version updated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   readmeUrl: 'https://raw.githubusercontent.com/gun88/fitnesse-ssh-fixture/master/README.md',
-  downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.0.0/fitnesse-ssh-fixture-1.0.0.jar',
+  downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.0.1/fitnesse-ssh-fixture-1.0.1.jar',
   gitHubUrl: 'https://github.com/gun88/fitnesse-ssh-fixture',
   wikiUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/wiki'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
     production: false,
     readmeUrl: 'https://raw.githubusercontent.com/gun88/fitnesse-ssh-fixture/master/README.md',
-  downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.0.0/fitnesse-ssh-fixture-1.0.0.jar',
+  downloadUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/releases/download/v1.0.1/fitnesse-ssh-fixture-1.0.1.jar',
     gitHubUrl: 'https://github.com/gun88/fitnesse-ssh-fixture',
     wikiUrl: 'https://github.com/gun88/fitnesse-ssh-fixture/wiki'
   }
